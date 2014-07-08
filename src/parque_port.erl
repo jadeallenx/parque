10878b59298b7edc6c41a747e6c612029ce962d4
Mark Allen <mrallen1@yahoo.com>
2014-07-08 18:20:23 -0500

    Update port to match player calls

--- src/parque_port.erl
+++ src/parque_port.erl
@@ -74,7 +74,7 @@ handle_call({arrive, Who}, _From, State = #state{name = Name, traders = Traders}
             Traders;
         false ->
             error_logger:info_msg("~p arrived at ~p.", [Who, Name]),
-            notify(Traders, {arrived, Who}),
+            notify(Traders, {arrived, Name, Who}),
             [Who | Traders]
     end,
     {reply, ok, State#state{traders = NewTraders}};
@@ -86,21 +86,20 @@ handle_call({leave, Who}, _From, State = #state{name = Name, traders = Traders})
             Traders;
         true ->
             error_logger:info_msg("~p left ~p.", [Who, Name]),
-            notify(Traders, {departed, Who}),
+            notify(Traders, {departed, Name, Who}),
             lists:delete(Who, Traders)
     end,
     {reply, ok, State#state{traders = NewTraders}};
 
-handle_call({buy, Who, What, Qty}, _From, State = #state{ goods = Goods, traders = Traders }) ->
+handle_call({buy, Who, What, Qty}, _From, State = #state{ name = Name, goods = Goods, traders = Traders }) ->
     {Return, NewState} = 
         case orddict:find(What, Goods) of
             {ok, {QtyAvailable, Price}} when QtyAvailable >= Qty -> 
-                case can_debit(Who, (Qty*Price)) of
+                case parque_player:afford(Who, (Qty*Price)) of
                     true ->
                         NewQty = QtyAvailable - Qty,
                         NewPrice = set_new_price(NewQty, QtyAvailable, Price),
-                        ok = debit(Who, Qty*Price),
-                        notify(Traders, {bought, Who, What, Qty, Price, NewQty, NewPrice}),
+                        notify(Traders, {bought, Name, Who, What, Qty, Price, NewQty, NewPrice}),
                         error_logger:info_msg("~p bought ~p ~p at ~p per unit (~p total).", [Who, Qty, What, Price, (Qty*Price)]),
                         {{ok, {Qty, Price}}, 
                             State#state{ goods = orddict:store(What, {NewQty, NewPrice}, Goods) }};
@@ -110,10 +109,10 @@ handle_call({buy, Who, What, Qty}, _From, State = #state{ goods = Goods, traders
                 end;
             {ok, {QtyAvailable, Price}} -> 
                 error_logger:error_msg("~p tried to buy ~p ~p at ~p per unit (~p total) but there's only ~p units of ~p available.", [Who, Qty, What, Price, (Qty*Price), QtyAvailable, What]),
-                {{error, not_enough_qty, QtyAvailable}, State};
+                {{error, {not_enough_qty, QtyAvailable}}, State};
             error -> 
                 error_logger:error_msg("~p tried to buy ~p ~p but there's no such good as ~p.", [Who, Qty, What, What]),
-                {{error, no_such_good, What}, State}
+                {{error, {no_such_good, What}}, State}
         end,
     {reply, Return, NewState};
 
@@ -121,23 +120,14 @@ handle_call({sell, Who, What, Qty}, _From, State = #state{ goods = Goods, trader
     {Return, NewState} = 
         case orddict:find(What, Goods) of
             {ok, {QtyAvailable, Price}} -> 
-                case has_inventory(Who, What, Qty) of
-                    true ->
-                        NewQty = QtyAvailable + Qty,
-                        NewPrice = set_new_price(NewQty, QtyAvailable, Price),
-                        ok = unload(Who, What, Qty),
-                        ok = credit(Who, Qty*Price),
-                        notify(Traders, {sold, Who, What, Qty, Price, NewQty, NewPrice}),
-                        error_logger:info_msg("~p sold ~p ~p at ~p per unit (~p total).", [Who, Qty, What, Price, (Qty*Price)]),
-                        {{ok, {Qty, Price}}, 
-                            State#state{ goods = orddict:store(What, {NewQty, NewPrice}, Goods) }};
-                    false ->
-                        error_logger:error_msg("~p tried to sell ~p ~p at ~p per unit (~p total) but doesn't have enough units.", [Who, Qty, What, Price, (Qty*Price)]),
-                        {{error, not_enough_inventory}, State}
-                end;
+                NewQty = QtyAvailable + Qty,
+                NewPrice = set_new_price(NewQty, QtyAvailable, Price),
+                notify(Traders, {sold, Name, Who, What, Qty, Price, NewQty, NewPrice}),
+                error_logger:info_msg("~p sold ~p ~p at ~p per unit (~p total).", [Who, Qty, What, Price, (Qty*Price)]),
+                {{ok, {Qty, Price}}, State#state{ goods = orddict:store(What, {NewQty, NewPrice}, Goods) }};
             error -> 
                 error_logger:error_msg("~p tried to sell ~p ~p but there's no such good as ~p at ~p.", [Who, Qty, What, What, Name]),
-                {{error, no_such_good, What}, State}
+                {{error, {no_such_good, What}}, State}
         end,
     {reply, Return, NewState};
  
@@ -162,21 +152,6 @@ code_change(_OldVsn, State, _Extra) ->
 %% private functions
 notify(Whom, Msg) ->
     lists:foreach(fun(Pid) -> gen_server:cast(Pid, Msg) end, Whom).
-
-can_debit(Who, Amount) ->
-    gen_server:call(Who, {afford, Amount}).
-
-has_inventory(Who, What, Qty) ->
-    gen_server:call(Who, {inventory, What, Qty}).
-
-unload(Who, What, Qty) ->
-    gen_server:call(Who, {unload, What, Qty}).
-
-debit(Who, Amount) ->
-    gen_server:call(Who, {debit, Amount}).
-
-credit(Who, Amount) ->
-    gen_server:call(Who, {credit, Amount}).
 
 set_new_price(0, _OldQty, OldPrice) ->
     OldPrice * 2;
